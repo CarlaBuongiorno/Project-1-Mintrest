3f90018f2813572d3b8f4ca13eae233740cc70e5
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-05-20 12:26:05 +0200

    Add date to every message

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 
+import datetime
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     with open('hello.txt', 'r') as file:
+        date = datetime.datetime.now()
         messages = [message.strip() for message in file]
-    return render_template('index.html', messages=messages)
+    return render_template('index.html', messages=messages, date=date.strftime("%d %B %Y"))
 
 
 @app.route('/newpost/')
