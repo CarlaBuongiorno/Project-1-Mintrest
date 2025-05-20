0e6d2421c670473ef8c946bf8c2ebeb8726981d4
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-05-20 14:19:43 +0200

    Get an accurate datetime rendered with each message

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     with open('hello.txt', 'r') as file:
-        date = datetime.datetime.now()
-        messages = [message.strip() for message in file]
-    return render_template('index.html', messages=messages, date=date.strftime("%d %B %Y"))
+        messages = [process_line(line) for line in file]
+    return render_template('index.html', messages=messages)
+
+
+def process_line(line):
+    '''Split each line at the first space and return the date string and message'''
+    time, msg = line.strip().split(' ', maxsplit=1)
+    date = datetime.datetime.fromtimestamp(float(time))
+    return date.strftime("%d %B %Y"), msg
 
 
 @app.route('/newpost/')
