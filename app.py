949555f66924d1b1de9bffaa09f2815b708baf21
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-05-26 16:12:51 +0200

    Process data from txt file to form usable records of pin data

--- app.py
+++ app.py
@@ -9,15 +9,39 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     with open('hello.txt', 'r') as file:
-        messages = [process_line(line) for line in file]
-    return render_template('index.html', messages=messages)
+        mintrest_data = file.readlines()
+        pins_data = process_line(mintrest_data)
+        pin_data = group_pins(pins_data)
+        pins = [create_pin(date=pin[0], title=pin[1], description=pin[2], image=pin[3]) for pin in pin_data]
+    return render_template('index.html', pins=pins)
 
 
-def process_line(line):
-    '''Split each line at the first space and return the date string and message'''
-    time, msg = line.strip().split(' ', maxsplit=1)
-    date = datetime.datetime.fromtimestamp(float(time))
-    return date.strftime("%d %B %Y"), msg
+def process_line(mintrest_data):
+    # Put all the lines into a single list
+    return [line.strip() for line in mintrest_data]
+
+
+def group_pins(pins):
+    # Separate the single list items into lists containing 4 items
+    # (title, description, image)
+    # Each list is 1 pin
+    return [pins[i:i+4] for i in range(0, len(pins), 4)]
+     
+
+def create_pin(date, title, description, image):
+    # Create a list dictionaries for each each pin)
+    return {
+        'date': get_date(date),
+        'title': title,
+        'description': description,
+        'image': image
+    }
+
+
+def get_date(date):
+    # Displays the recorded date
+    date = datetime.datetime.fromtimestamp(int(date))
+    return date.strftime("%d %B %Y")
 
 
 @app.route('/newpost/')
